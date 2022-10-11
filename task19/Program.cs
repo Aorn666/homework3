426935e648e1bb181da72572c266c32ab8f432fe
Ilya Matveev01~ <aorn666@gmail.com>
2022-10-11 16:19:39 +0300

    поприл

--- task19/Program.cs
+++ task19/Program.cs
@@ -4,18 +4,14 @@ Console.Clear();
 Console.Write("Введите 5и значное число:");
 
 int a = Convert.ToInt32(Console.ReadLine());
-int b = 0;
-int c = 0;
 
 if (a < 10000 || a > 100000){
  Console.WriteLine("число введено неверно");
 }
 else {
-  b = a % 10;
-  c = (a % 100)/10;
-}
-
-if (a / 10000 == b && (a / 1000)%10==c)
+  if (a / 10000 == a % 10 && (a / 1000)%10==(a % 100)/10)
  Console.WriteLine("да");
  else
   Console.WriteLine("нет");
+}
+
